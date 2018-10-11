5923fd8b6b57f0e541d77ff872c60a17c24794be
DavidSkratch <davidlc.91@hotmail.com>
2018-10-11 08:58:11 +0200

    buscador

--- src/dao/AnimalDAO.java
+++ src/dao/AnimalDAO.java
@@ -3,7 +3,6 @@ package dao;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
-//import java.util.Iterator; //Usado no activado
 import java.util.List;
 import java.util.Map;
 import domain.*;
@@ -16,21 +15,21 @@ public class AnimalDAO implements IAnimalDAO{
 	
 	@Override // Listado
 	public ArrayList<Animal> listarAnimalesByCategoria(Categoria actual) {
-		ArrayList<Animal> busqueda;
+		ArrayList<Animal> busqueda = new ArrayList<Animal>();
 		
 		return busqueda;
 	}
 
 	@Override //Detalle
 	public ArrayList<Animal> listarAnimalByID(int id) {
-		ArrayList<Animal> busqueda;
+		ArrayList<Animal> busqueda = new ArrayList<Animal>();
 		
 		return busqueda;
 	}
 	
 	@Override // Buscador
 	public ArrayList<Animal> listarAnimalesByPalabra(String palabra) {
-		ArrayList<Animal> busqueda;
+		ArrayList<Animal> busqueda = new ArrayList<Animal>();
 		
 		return busqueda;
 	}
@@ -57,7 +56,7 @@ public class AnimalDAO implements IAnimalDAO{
 	
 	public Animal veranimal(Animal nuevo)throws ClassNotFoundException, SQLException{
 		String query= "SELECT * FROM animales where ID_ANIMAL="+nuevo.getId();
-		java.sql.ResultSet resultadoquery = conector.mostraranimal(query);
+		java.sql.ResultSet resultadoquery = conector.mostrar(query);
 		Animal encontrado = new Animal();
 		while(resultadoquery.next()){
 			encontrado.setId(resultadoquery.getInt("ID_ANIMAL"));
@@ -71,47 +70,5 @@ public class AnimalDAO implements IAnimalDAO{
 		return encontrado;
 		
 	}
-	
-//	public List<Animal> getAnimales(String categoria){
-//		
-//		List<Animal> animales = new ArrayList<>();
-//		
-//		if(categoria.equals("invisibles")){
-//			animales.add(new Animal("Grumpy Cat"));
-//			animales.add(new Animal("Felix El Gato"));
-//		}else{
-//			animales.add(new Animal("Garfield"));
-//		}
-//		
-//		return (animales);
-//		
-//	}
-
-	/* Revisar
-	@Override
-	public boolean listarAnimales() {
-		Integer id;
-		Iterator<Integer> animales = animalDAO.keySet().iterator();
-		System.out.println("Listando animales...");
-		while(((Iterator<Integer>) animalStock).hasNext()){
-			id = animales.next();
-			System.out.println("[" + id + "]: " + animalStock.get(id));
-		}
-	}*/
-
-	public List<Animal> getAnimales(String nombre) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <e> ArrayList<Animal> listarAnimales(e o) {
-		// TODO Auto-generated method stub
-ArrayList<Animal> busqueda;
-		
-		return busqueda;
-	}
-
-
 
 }
--- src/dao/IAnimalDAO.java
+++ src/dao/IAnimalDAO.java
@@ -2,14 +2,11 @@ package dao;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
 
 import domain.Animal;
 import domain.Categoria;
 
 public interface IAnimalDAO {
-	
-	public List<Animal> getAnimales(String nombre);
 	
 	public boolean anadirAnimal(Animal animal) throws ClassNotFoundException, SQLException;
 	public boolean editarAnimal(Animal animal) throws ClassNotFoundException, SQLException;
