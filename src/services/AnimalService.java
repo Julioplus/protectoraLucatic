eacdeea36590ddd4f093ee8a40756ffdc9b16079
DavidSkratch <davidlc.91@hotmail.com>
2018-10-11 11:21:48 +0200

    animalService

--- src/services/AnimalService.java
+++ src/services/AnimalService.java
@@ -1,10 +1,8 @@
 package services;
 
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Time;
 import java.util.ArrayList;
-import java.util.List;
 import dao.AnimalDAO;
 import dao.IAnimalDAO;
 import domain.Animal;
@@ -14,13 +12,6 @@ import domain.Descripcion;
 public class AnimalService implements IAnimalService {
 	
 	private IAnimalDAO AnimalDAO = new AnimalDAO();
-	/*
-	private String nombre;
-	private Descripcion descripcion;	
-	private Time fechaEntrada;
-	private Time fechaSalida;
-	private Categoria estado;
-	 */
 	
 	@Override // Listado
 	public ArrayList<Animal> listarAnimalesByCategoria(String categoria) {
@@ -50,7 +41,7 @@ public class AnimalService implements IAnimalService {
 	}
 
 	@Override //Detalle
-	public ArrayList<Animal> listarAnimalByID(int id) {
+	public Animal listarAnimalByID(int id) {
 		ArrayList<Animal> busqueda = new ArrayList<Animal>();
 		try {
 			busqueda = AnimalDAO.listarAnimalByID(id);
@@ -59,7 +50,7 @@ public class AnimalService implements IAnimalService {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return busqueda;
+		return busqueda.get(0);
 			
 	}
 	
@@ -90,23 +81,5 @@ public class AnimalService implements IAnimalService {
 		Animal nuevo = new Animal(entrada[0], new Descripcion(entrada[1], entrada[2]), Time.valueOf(entrada[3]), Categoria.valueOf(entrada[4]));
 		return true;
 	}
-	
-	
-	
-	
-	@Override
-	public ArrayList<Animal> getAnimales(String categoria) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	@Override
-	public Animal getAnimal(int id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-//    public List<Animal> getAnimales(String categoria) {
-//       return AnimalDAO.getAnimales(categoria);
-//    }
 
 }
--- src/services/IAnimalService.java
+++ src/services/IAnimalService.java
@@ -1,17 +1,17 @@
 
 package services;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import domain.Animal;
 
 public interface IAnimalService {
 	
-	public ArrayList<Animal> getAnimales(String categoria);
 	public ArrayList<Animal> listarAnimalesByCategoria(String categoria);
-	public ArrayList<Animal> listarAnimalByID(int id);
+	public Animal listarAnimalByID(int id);
 	public ArrayList<Animal> listarAnimalesByPalabra(String palabra);
+	public boolean anadirAnimal(String[] entrada) throws ClassNotFoundException, SQLException;
+	public boolean editarAnimal(String[] entrada) throws ClassNotFoundException, SQLException;
+	public boolean eliminarAnimal(String[] entrada) throws ClassNotFoundException, SQLException;
 
-	public Animal getAnimal(int id) {
-		
-	}
 }
